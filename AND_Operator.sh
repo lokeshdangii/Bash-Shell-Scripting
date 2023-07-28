96d4b86aeb24c582dc4e24e9521f50fe1b6053f8
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-28 14:42:15 +0530

    AND operator in Bash Scripting

---
+++ AND_Operator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+age=25
+
+# if [ "$age" -gt 18 ] && [ "$age" -lt 30 ]
+
+if [ "$age" -gt 18 -a "$age" -lt 30 ]
+then
+    echo "Valid Age"
+else
+    echo "NOT Valid Age"
+fi
+
+# AND Operator --> && or -a
+
+#  && --> use two Sq. Brackets [] && []
+#  -a --> use one Sq. Bracket [ cond1 -a cond2 ]
