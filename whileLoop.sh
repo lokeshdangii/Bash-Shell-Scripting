385c2baa88a65aa5d30c04cb91e99bc77020b9b9
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-28 21:36:55 +0530

    while loop

---
+++ whileLoop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+n=1
+
+# while [ $n -le 10 ]
+
+while (( $n <= 10 ))
+do
+    echo "$n"
+    # n=$(( n+1 ))
+    (( n++ ))
+    gnome-terminal &  # will open the terminal
+    sleep 1
+done  
