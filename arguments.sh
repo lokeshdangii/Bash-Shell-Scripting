b0d8a643d8624483f93005532796bd8a852b4d7f
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-28 12:54:29 +0530

    Arguments in Bash Scripting

---
+++ arguments.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# $0 --> Script name
+# argumets stored from $1 to $9
+
+echo $0 $1 $2 $3
+
+# $@ stores arguments as an array
+args=("$@") 
+
+
+echo ${args[0]} ${args[1]} ${args[2]} 
+# the first argument you pass will assign to zero index
+
+#  to print all the arguments in one go
+echo $@
+
+# to print the number of arguments passed in the Script
+echo $#
