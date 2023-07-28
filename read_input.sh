9f383d1e502a0f7324a3ea3b797e808b4a737985
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-28 12:35:28 +0530

    Read input in Bash Scripting

---
+++ read_input.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# echo "Enter name :"
+# read name1 name2 name3 name4
+# echo "Entered name :- $name1, $name2, $name3, $name4"
+
+
+# to read input on the same line
+
+# read -p 'username : ' user_var
+# read -sp 'password : ' pass_var  # hide the input we provide on terminal
+# echo "username : $user_var"      # take one input only
+# echo "password : $pass_var"
+
+
+echo "Enter names :"
+read -a names     # -a flag takes input as array
+echo ${names[0]} ${names[1]} ${names[2]} 
