3cb4cc6cf5ad3eac1794e32cbf61d89530f00460
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-28 16:38:42 +0530

    Array and array variable in Bash

---
+++ array_variables.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Defining an array
+
+os=('ubuntu' 'windows' 'Kali' 'Mac')
+os[4]="Android"   # adding element
+os[0]="linux " # updating element
+
+unset os[2]   # removing element
+
+# echo ${os[0]} ${os[1]} ${os[3]} ${os[2]} 
+echo ${os[@]}  # @ will return all elements
+
+echo ${!os[@]} # prints indices of array
+
+echo ${#os[@]} # print length of array
+
+
+# variables behave as array
+
+str="itisastring"
+# echo ${str[@]}
+echo ${str[0]}
+echo ${str[1]}
+echo ${#str[@]}
