0455668e567fcdbbed346fa06e5940c66a987097
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-28 14:30:51 +0530

    appending to a file in Scripting

---
+++ append_to_file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ 
+echo -e "Enter the name of the file : \c"
+read file_name
+
+if [ -f $file_name ]
+then 
+    if [ -w $file_name ]
+    then
+        echo "Type some text data. To quit press Ctrl + d"
+        cat >>  $file_name
+        # single bracket (>) the file will be overwritten
+        # double bracket (>>) the file will be appended
+    else
+        echo "The $file_name doesn't have write permission"
+    fi
+
+else
+    echo "$file_name not exists"
+fi
