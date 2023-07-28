cf086abf45309c0905d182d9d6709962ecf93d91
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-28 14:59:00 +0530

    OR Operator in Bash

---
+++ OR_Operator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+age=25
+
+# if [ "$age" -gt 18 ] && [ "$age" -lt 30 ]
+# if [[ "$age" -gt 18 && "$age" -lt 30 ]]
+
+if [ "$age" -eq 18 -o "$age" -eq 30 ]
+then
+    echo "Valid Age"
+else
+    echo "NOT Valid Age"
+fi
+
+# OR Operator --> || or -0
+
+#  || --> use two Sq. Brackets [] || []  or [[ Cond1 || Cond2 ]]
+#  -o --> use one Sq. Bracket [ cond1 -0 cond2 ]
