fcc41575aa2b1ed6a697bbc97443e05e9df2c8ef
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-30 22:35:53 +0530

    signal and trap in Bash Scripting

---
+++ signal_and_trap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# trap "echo Exit command is detected" 0
+
+# echo "Hello World"
+
+# exit 0
+
+# 0 means success
+
+trap " echo Exit singal is detected" SIGINT   # SIGKILL --> 9 SIGINT --> 2
+
+echo "pid is $$"
+while (( COUNT < 10 ))
+do
+    sleep 10
+    ((COUNT ++ ))
+    echo $COUNT
+done
+exit 0
