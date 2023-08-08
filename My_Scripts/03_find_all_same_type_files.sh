4daa61b984101a9c889de467628b4fecb09509b5
lokeshdangii <lokeshdangi1045@gmail.com>
2023-08-08 18:19:32 +0530

    Script to find files of same type

---
+++ My_Scripts/03_find_all_same_type_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script to find the file of same type like (.py, .txt, .json etc) at a provided path
+
+read -p "Enter the extension of type of file want to find : " filename
+read -p "Enter the path where you want to find : " path
+
+
+if [ -d $path ]
+then 
+    files=( $( find $path -name "*$filename" ) )
+    if [ $files ]
+    then
+        for (( i=0; i<${#files[@]}; i++ )) 
+        do
+            echo "${files[i]}"  
+        done
+        
+    else
+        echo "file with $filename not found"
+    fi
+else
+    echo "Wrong path .....!!!"
+    exit
+fi
+
+
